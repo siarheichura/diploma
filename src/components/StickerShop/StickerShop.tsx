4040200ac3f89ce92c8b8a579a50204948527571
siarheichura <chura14@yandex.ru>
2021-10-23 02:24:57 +0300

    che-to

--- src/components/StickerShop/StickerShop.tsx
+++ src/components/StickerShop/StickerShop.tsx
@@ -1,14 +1,15 @@
 import styles from "./StickerShop.module.css";
 
 export function StickerShop() {
+
   return (
     <div className={styles.stickerShop}>
       <div>Buy my Sticker on your car.</div>
 
       <div className={styles.shopContainer}>
-        <div className={styles.shopItem}>insta Nikname</div>
-        <div className={styles.shopItem}>car Zvonilki</div>
-        <div className={styles.shopItem}>Reklama Na Zadnee Steklo</div>
+        <div className={`${styles.shopItem} ${styles.insta}`}>instagram nickname</div>
+        <div className={`${styles.shopItem} ${styles.call}`}>call me</div>
+        <div className={`${styles.shopItem} ${styles.window}`}>rear window advertisement</div>
         <div className={styles.shopItem}>Tvoi Variantik</div>
       </div>
     </div>
