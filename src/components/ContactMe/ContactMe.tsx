76c5aa441f98c541363c807d15e08d01f4d673d8
siarheichura <chura14@yandex.ru>
2021-10-23 01:06:15 +0300

    add styles, refactor, add tgBot message

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
 import styles from "./ContactMe.module.css";
 
 export function ContactMe() {
+  const [inputNameValue, setInputNameValue] = useState("");
+  const [inputEmailValue, setInputEmailValue] = useState("");
+  const [inputMessageValue, setInputMessageValue] = useState("");
+
+  const tgBotData = {
+    botToken: "2069403715:AAFZ-0LpT7i2L0sqjNOsw7ccsChvJK93_Lo",
+    chatId: -463402438,
+  };
+
+  const sendMessage = () => {
+    let { botToken, chatId } = tgBotData;
+    let message = `<b>Name:</b> ${inputNameValue}%0A<b>Email:</b> ${inputEmailValue}%0A<b>Message:</b> ${inputMessageValue}`
+      // <b>Email:</b> <i>${inputEmailValue}</i>. %0A 
+      // <b>Message:</b> <i>${inputMessageValue}</i>.`
+    let url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=html`
+    fetch(url)
+    setInputNameValue('')
+    setInputEmailValue("")
+    setInputMessageValue("")
+  }
+
+  const onInputNameChange = (event: any) => {
+    setInputNameValue(event.target.value);
+  };
+  const onInputEmailChange = (event: any) => {
+    setInputEmailValue(event.target.value);
+  };
+  const onInputMessageChange = (event: any) => {
+    setInputMessageValue(event.target.value);
+  };
+
   return (
   <div className={styles.contactMe}>
     <h2 className={styles.title}>
@@ -9,10 +41,21 @@ export function ContactMe() {
     
     </h2>
     <p className={styles.text}>Job offers, new projects, freelance inquiry or even a cup of coffee.</p>
-    <input type="text" />
-    <input type="text" />
-    <textarea></textarea>
-    <button>Send Message</button>
+    <label>
+      Name
+      <input type="text" value={inputNameValue} onChange={onInputNameChange}  />
+    </label>
+    <label>
+      E-mail
+      <input type="text" value={inputEmailValue} onChange={onInputEmailChange} />
+    </label>
+    <label>
+      Message
+      <textarea value={inputMessageValue} onChange={onInputMessageChange}></textarea>
+    </label>
+
+
+    <button onClick={sendMessage}>Send Message</button>
   </div>
   )
 }
--- src/components/StickerShop/StickerShop.tsx
+++ src/components/StickerShop/StickerShop.tsx
@@ -1,5 +1,17 @@
 import styles from "./StickerShop.module.css";
 
 export function StickerShop() {
-  return <div className={styles.stickerShop}>Sticker Shop</div>;
+  return (
+    <div className={styles.stickerShop}>
+      <div>Buy my Sticker on your car.</div>
+
+      <div className={styles.shopContainer}>
+        <div className={styles.shopItem}>insta Nikname</div>
+        <div className={styles.shopItem}>car Zvonilki</div>
+        <div className={styles.shopItem}>Reklama Na Zadnee Steklo</div>
+        <div className={styles.shopItem}>Tvoi Variantik</div>
+      </div>
+    </div>
+  )
+
 }
